b9d3e7745972b77949063622e91f1700956c0ef7
Temitope <temmieayodele@gmail.com>
2019-09-07 15:01:38 +0100

    changed class component to functional component and introduced hooks

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
 import { compose } from 'recompose';
@@ -7,97 +7,101 @@ import * as ROUTES from '../constants/routes';
 const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
-    <SignUpForm/>
+    <SignUpForm />
   </div>
 );
-const INITIAL_STATE = {
+const initialState = {
   username: '',
   email: '',
   passwordOne: '',
   passwordTwo: '',
   error: null,
 };
-class SignUpFormBase extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...INITIAL_STATE }
-  }
-  onSubmit = event => {
-    const { username, email, passwordOne } = this.state;
-    this.props.firebase
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
+const SignUpFormBase = (props) => {
+  const [userDetails, setUserDetails] = useState({ ...initialState })
+
+  const onSubmit = event => {
+    const { username, email, passwordOne } = userDetails;
+    props.firebase
+      .createUser(email, passwordOne)
       .then(authUser => {
         // Create a user in your Firebase realtime database
-        return this.props.firebase
+        return props.firebase
           .user(authUser.user.uid)
           .set({
             username,
             email,
           });
       })
-      .then(authUser => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
+      .then(() => {
+        setUserDetails({ ...initialState })
+        props.history.push(ROUTES.HOME);
       })
       .catch(error => {
-        this.setState({ error });
+        setUserDetails({ ...userDetails, error: error })
       });
     event.preventDefault();
   }
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const onChange = event => {
+    setUserDetails({ ...userDetails, [event.target.name]: event.target.value })
   };
-  render() {
-    const {
-      username,
-      email,
-      passwordOne,
-      passwordTwo,
-      error,
-    } = this.state;
+  const {
+    username,
+    email,
+    passwordOne,
+    passwordTwo,
+    error,
+  } = userDetails;
 
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === '' ||
-      email === '' ||
-      username === '';
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === '' ||
+    email === '' ||
+    username === '';
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">Sign Up</button>
-        {error && <p>{error.message}</p>}
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={(e) => { onSubmit(e) }}>
+      <input
+        name="username"
+        value={username}
+        onChange={e => {
+          onChange(e);
+        }}
+        type="text"
+        placeholder="Full Name"
+      />
+      <input
+        name="email"
+        value={email}
+        onChange={e => {
+          onChange(e);
+        }}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="passwordOne"
+        value={passwordOne}
+        onChange={e => {
+          onChange(e);
+        }}
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={e => {
+          onChange(e);
+        }}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <button disabled={isInvalid} type="submit">Sign Up</button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
 }
 const SignUpLink = () => (
   <p>
