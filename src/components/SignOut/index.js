4dd7486284887c02847d0c911a6f7342033225df
Temitope <temmieayodele@gmail.com>
2019-09-07 15:01:26 +0100

    changed class component to functional component and introduced hooks

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withFirebase } from '../Firebase';
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
+  <button type="button" onClick={firebase.signOutUser}>
     Sign Out
   </button>
 );
